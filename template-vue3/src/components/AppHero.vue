3b174ed19fc572b23091201ed1ef4b408d9955bb
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-02-06 20:35:34 +0100

    exercise completed with bonus

---
+++ template-vue3/src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name: "AppHero"
+}
+</script>
+
+<template>
+ <img width="50%" src="../assets/young-guy-is-working-on-a-laptop-tired-character-concept-of-eye-health-while-working-at-the-computer-flat-graphics-illustration-vector.jpg" alt="">
+</template>
+
+<style scoped>
+
+</style>
